244d820e974525755ee7219a0e5e601750ec66d4
Teza Alfian <tezaalfian2916@gmail.com>
2022-05-25 11:08:10 +0700

    add activity food list

--- app/src/main/java/com/tezaalfian/simpasi/view/main/food/list/FoodListActivity.kt
+++ app/src/main/java/com/tezaalfian/simpasi/view/main/food/list/FoodListActivity.kt
@@ -1,12 +1,12 @@
-package com.tezaalfian.simpasi.view.fooddetail
+package com.tezaalfian.simpasi.view.main.food.list
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.tezaalfian.simpasi.R
 
-class FoodDetailActivity : AppCompatActivity() {
+class FoodListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_food_detail)
+        setContentView(R.layout.activity_food_list)
     }
 }
