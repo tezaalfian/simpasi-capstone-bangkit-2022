1294ba88064fcbe0b84828916c14c590f06d64b5
Teza Alfian <tezaalfian2916@gmail.com>
2022-05-25 10:57:36 +0700

    add activity food detail

---
+++ app/src/main/java/com/tezaalfian/simpasi/view/fooddetail/FoodDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.tezaalfian.simpasi.view.fooddetail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tezaalfian.simpasi.R
+
+class FoodDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_food_detail)
+    }
+}
