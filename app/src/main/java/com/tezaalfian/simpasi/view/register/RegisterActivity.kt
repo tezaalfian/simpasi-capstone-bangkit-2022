f6003c3dbf69c7ab3ebfced850faeb77be6c7847
Dzikril <daengdziha77@gmail.com>
2022-05-27 22:26:31 +0800

    Add register activity

---
+++ app/src/main/java/com/tezaalfian/simpasi/view/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.tezaalfian.simpasi.view.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tezaalfian.simpasi.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
