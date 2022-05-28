936e5c1e6574c4edb59c774f2aa343cbf26105f6
Teza Alfian <tezaalfian2916@gmail.com>
2022-05-28 16:50:20 +0700

    Add ChildResponse.kt

---
+++ app/src/main/java/com/tezaalfian/simpasi/core/data/source/remote/response/ChildResponse.kt
@@ -0,0 +1,36 @@
+package com.tezaalfian.simpasi.core.data.source.remote.response
+
+import com.google.gson.annotations.SerializedName
+
+data class ChildResponse(
+
+	@field:SerializedName("_id")
+	val id: String? = null,
+
+	@field:SerializedName("nama")
+	val nama: String? = null,
+
+	@field:SerializedName("tglLahir")
+	val tglLahir: String? = null,
+
+	@field:SerializedName("umur")
+	val umur: Int? = null,
+
+	@field:SerializedName("tb_bayi")
+	val tbBayi: Int? = null,
+
+	@field:SerializedName("bb_bayi")
+	val bbBayi: Int? = null,
+
+	@field:SerializedName("alergi")
+	val alergi: String? = null,
+
+	@field:SerializedName("user")
+	val user: String? = null,
+
+	@field:SerializedName("jk_bayi")
+	val jkBayi: String? = null,
+
+	@field:SerializedName("tglTerdaftar")
+	val tglTerdaftar: String? = null
+)
