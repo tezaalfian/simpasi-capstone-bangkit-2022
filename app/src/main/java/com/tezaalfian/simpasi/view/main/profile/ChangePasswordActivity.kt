cc21bf90c4b3436da1f70cb03f3c8f959764d075
Dzikril <daengdziha77@gmail.com>
2022-06-03 21:30:23 +0700

    add Change Password Activity

---
+++ app/src/main/java/com/tezaalfian/simpasi/view/main/profile/ChangePasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.tezaalfian.simpasi.view.main.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tezaalfian.simpasi.R
+
+class ChangePasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_password)
+    }
+}
