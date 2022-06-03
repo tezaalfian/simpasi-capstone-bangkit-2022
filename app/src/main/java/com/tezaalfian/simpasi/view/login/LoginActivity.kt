e0306e5e9dc32491a19b33755457b426d3423751
Dzikril <daengdziha77@gmail.com>
2022-06-03 17:10:25 +0700

    edit layout for edit profile activity

---
+++ app/src/main/java/com/tezaalfian/simpasi/view/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.tezaalfian.simpasi.view.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tezaalfian.simpasi.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
