bb27d08228dcbc230aa9793a2f521b260107fccb
Dzikril <daengdziha77@gmail.com>
2022-06-06 19:31:45 +0700

    add RegisterViewModel and RegisterActivity

--- app/src/main/java/com/tezaalfian/simpasi/view/register/RegisterActivity.kt
+++ app/src/main/java/com/tezaalfian/simpasi/view/register/RegisterActivity.kt
@@ -2,11 +2,115 @@ package com.tezaalfian.simpasi.view.register
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.tezaalfian.simpasi.R
+import com.tezaalfian.simpasi.core.data.Resource
+import com.tezaalfian.simpasi.core.utils.animateVisibility
+import com.tezaalfian.simpasi.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var registerViewModel: RegisterViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        setupAction()
+        signUp()
+    }
+
+    private fun setupAction(){
+        binding.btnSignup.setOnClickListener{
+            signUp()
+        }
+
+        binding.tvSignupLogin.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        binding.apply {
+            edtSignupEmail.isEnabled = !isLoading
+            edtSignupPassword.isEnabled = !isLoading
+            edtSignupName.isEnabled = !isLoading
+            edtSignupUsername.isEnabled = !isLoading
+            edtSignupConfirmPassword.isEnabled = !isLoading
+            btnSignup.isEnabled = !isLoading
+
+            if (isLoading) {
+                viewProgressbar.animateVisibility(true)
+            } else {
+                viewProgressbar.animateVisibility(false)
+            }
+        }
+    }
+
+    private fun signUp(){
+        val name = binding.edtSignupName.text.toString().trim()
+        val email = binding.edtSignupEmail.text.toString().trim()
+        val username = binding.edtSignupUsername.text.toString().trim()
+        val password = binding.edtSignupPassword.text.toString().trim()
+        val confirmPassword = binding.edtSignupConfirmPassword.text.toString().trim()
+
+        when {
+            name.isEmpty() -> {
+                binding.edtSignupName.error =
+                    resources.getString(R.string.message_validation, "name")
+            }
+            email.isEmpty() -> {
+                binding.edtSignupEmail.error =
+                    resources.getString(R.string.message_validation, "email")
+            }
+            username.isEmpty() -> {
+                binding.edtSignupUsername.error =
+                    resources.getString(R.string.message_validation, "username")
+            }
+            password.isEmpty() -> {
+                binding.edtSignupPassword.error =
+                    resources.getString(R.string.message_validation, "password")
+            }
+            confirmPassword.isEmpty() -> {
+                binding.edtSignupConfirmPassword.error =
+                    resources.getString(R.string.message_validation, "confirm password")
+            }
+            else -> {
+                registerViewModel.register(name, email, username, password, confirmPassword).observe(this){ result ->
+                    if (result != null){
+                        when(result) {
+                            is Resource.Loading -> {
+                                showLoading(true)
+                            }
+                            is Resource.Success -> {
+                                showLoading(false)
+                                val user = result.data
+                                    AlertDialog.Builder(this@RegisterActivity).apply {
+                                        setTitle(getString(R.string.tittle_alert))
+                                        setMessage(getString(R.string.message_success_alert))
+                                        setPositiveButton(getString(R.string.next)) { _, _ ->
+                                            finish()
+                                        }
+                                        create()
+                                        show()
+                                    }
+                            }
+                            is Resource.Error -> {
+                                showLoading(false)
+                                Toast.makeText(
+                                    this,
+                                    result.error,
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/tezaalfian/simpasi/view/register/RegisterViewModel.kt
+++ app/src/main/java/com/tezaalfian/simpasi/view/register/RegisterViewModel.kt
@@ -1,9 +1,10 @@
 package com.tezaalfian.simpasi.view.register
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.tezaalfian.simpasi.core.data.source.repository.UserRepository
 
 class RegisterViewModel (private val repo: UserRepository) : ViewModel() {
-    fun register(nama: String, email: String, username: String, password: String, confirm_password: String)
-    = repo.register(nama, email, username, password, confirm_password)
+    fun register(name: String, email: String, username: String, password: String, confirm_password: String)
+    = repo.register(name, email, username, password, confirm_password).asLiveData()
 }
