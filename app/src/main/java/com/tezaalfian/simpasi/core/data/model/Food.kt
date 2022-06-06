ddf95744987614c0e2d026cf6e67fd2a552e18d8
Teza Alfian <tezaalfian2916@gmail.com>
2022-06-06 13:03:20 +0700

    add ml model

---
+++ app/src/main/java/com/tezaalfian/simpasi/core/data/model/Food.kt
@@ -0,0 +1,6 @@
+package com.tezaalfian.simpasi.core.data.model
+
+data class Food (
+    val id: String = "tes",
+    val nama_resep: String = "Apel Rebus"
+)
--- app/src/main/java/com/tezaalfian/simpasi/view/main/home/HomeFragment.kt
+++ app/src/main/java/com/tezaalfian/simpasi/view/main/home/HomeFragment.kt
@@ -1,16 +1,32 @@
 package com.tezaalfian.simpasi.view.main.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import com.google.firebase.ml.modeldownloader.CustomModel
+import com.google.firebase.ml.modeldownloader.CustomModelDownloadConditions
+import com.google.firebase.ml.modeldownloader.DownloadType
+import com.google.firebase.ml.modeldownloader.FirebaseModelDownloader
+import com.tezaalfian.simpasi.core.data.model.Food
+import com.tezaalfian.simpasi.core.utils.Config
+import com.tezaalfian.simpasi.core.utils.RecommendationClient
 import com.tezaalfian.simpasi.databinding.FragmentHomeBinding
+import kotlinx.coroutines.launch
+import org.tensorflow.lite.Interpreter
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
+
+    private lateinit var interpreter: Interpreter
+
+    private var config = Config()
+    private var client: RecommendationClient? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,10 +38,59 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this)[HomeViewModel::class.java]
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        lifecycleScope.launch {
+            // Run inference with TF Lite.
+            Log.d(TAG, "Run inference with TFLite model.")
+            val movies = listOf(Food())
+            client?.recommend(movies)?.run {
+                Log.d(TAG, toString())
+            }
+        }
+//        val conditions = CustomModelDownloadConditions.Builder()
+//            .requireWifi()  // Also possible: .requireCharging() and .requireDeviceIdle()
+//            .build()
+//        FirebaseModelDownloader.getInstance()
+//            .getModel("simpasi", DownloadType.LOCAL_MODEL_UPDATE_IN_BACKGROUND,
+//                conditions)
+//            .addOnSuccessListener { model: CustomModel? ->
+//                val modelFile = model?.file
+//                if (modelFile != null) {
+//                    interpreter = Interpreter(modelFile)
+//
+//                    val input = arrayOf("tes")
+//                    val output: MutableMap<Int, Any> = HashMap()
+//
+//                    interpreter.runForMultipleInputsOutputs(input, output)
+////                    val modelOutput = ByteBuffer.allocateDirect(bufferSize).order(ByteOrder.nativeOrder())
+////                    interpreter.run(input, output)
+//                    Log.d("MLModel", output.toString())
+//                }
+//            }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        lifecycleScope.launch {
+            client?.load()
+        }
+    }
+
+    override fun onStop() {
+        lifecycleScope.launch {
+            client?.unload()
+        }
+        super.onStop()
+    }
+
+    companion object {
+        private val TAG = "HomeFragment";
     }
 
     override fun onDestroyView() {
