ad2196f9fc1d4871712b6e97bbf53396351c4c68
Teza Alfian <tezaalfian2916@gmail.com>
2022-06-10 23:35:47 +0700

    read model ml success

--- app/src/main/java/com/tezaalfian/simpasi/view/main/home/HomeFragment.kt
+++ app/src/main/java/com/tezaalfian/simpasi/view/main/home/HomeFragment.kt
@@ -5,16 +5,20 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.google.firebase.ml.modeldownloader.CustomModel
 import com.google.firebase.ml.modeldownloader.CustomModelDownloadConditions
 import com.google.firebase.ml.modeldownloader.DownloadType
 import com.google.firebase.ml.modeldownloader.FirebaseModelDownloader
-import com.tezaalfian.simpasi.core.data.model.Food
+import com.tezaalfian.simpasi.core.data.source.local.entity.FoodEntity
+import com.tezaalfian.simpasi.core.ui.ListFoodAdapter
 import com.tezaalfian.simpasi.databinding.FragmentHomeBinding
 import org.tensorflow.lite.Interpreter
+import java.io.BufferedReader
 import java.io.IOException
+import java.io.InputStreamReader
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 
@@ -24,87 +28,76 @@ class HomeFragment : Fragment() {
 
     private lateinit var interpreter: Interpreter
     // onDestroyView.
-    private val binding get() = _binding!!
+    private val binding get() = _binding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): NestedScrollView? {
         val homeViewModel =
             ViewModelProvider(this)[HomeViewModel::class.java]
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        return binding.root
+        return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        lifecycleScope.launch {
-//            client?.load()
-//            val recommendations: List<Result> = client!!.recommend(listOf(Food()))
-//            Log.d("HomeFragment", recommendations.toString())
-//        }
-//        lifecycleScope.launch {
-//            // Run inference with TF Lite.
-//            Log.d(TAG, "Run inference with TFLite model.")
-//            val movies = listOf(Food())
-//            client?.recommend(movies)?.run {
-//                Log.d(TAG, toString())
-//            }
-//        }
-
         val conditions = CustomModelDownloadConditions.Builder()
             .build()
         FirebaseModelDownloader.getInstance()
             .getModel("simpasi", DownloadType.LOCAL_MODEL_UPDATE_IN_BACKGROUND,
                 conditions)
-            .addOnFailureListener {
-                it.printStackTrace()
-                Log.d(TAG+"Error", it.message.toString())
-            }
             .addOnSuccessListener { model: CustomModel? ->
                 val modelFile = model?.file
                 if (modelFile != null) {
                     interpreter = Interpreter(modelFile)
-                    val input = arrayOf(Food().userId)
-//                    val output: MutableMap<Int, Any> = HashMap()
-//                    interpreter.runForMultipleInputsOutputs(input, output)
-//                    val bufferSize = 1000 * java.lang.Float.SIZE / java.lang.Byte.SIZE
 
-//                    val input: ByteBuffer = ByteBuffer.allocateDirect(1).order(ByteOrder.nativeOrder())
-//                    input.put(Food().userId.toByte())
-                    val bufferSize = 1000 * java.lang.Float.SIZE / java.lang.Byte.SIZE
-                    val output = ByteBuffer.allocateDirect(bufferSize).order(ByteOrder.nativeOrder())
-//                    val output = listOf<String>()
-                    // Creates inputs for reference.
-//                    val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 3), DataType.INT32)
-//                    inputFeature0.loadBuffer(byteBuffer)
-//
-//                    // Runs model inference and gets result.
-//                    val outputs = interpreter.process
-//                    val outputFeature0 = outputs.outputFeature0AsTensorBuffer.floatArray
+                    val input = ByteBuffer.allocateDirect(8).order(ByteOrder.nativeOrder())
+                    input.putFloat(8f)
+                    input.putFloat(1f)
 
-//                    val bufferSize = 1000 * java.lang.Float.SIZE / java.lang.Byte.SIZE
-                    interpreter.run(input, output)
-//                    interpreter.runForMultipleInputsOutputs(input, output)
-//                    Log.d(TAG, output.float.toString())
-                    output.rewind()
-//                    val probabilities = output.asFloatBuffer()
+                    val bufferSize = 22 * java.lang.Float.SIZE / java.lang.Byte.SIZE
+                    val modelOutput = ByteBuffer.allocateDirect(bufferSize).order(ByteOrder.nativeOrder())
+                    interpreter.run(input, modelOutput)
+                    modelOutput.rewind()
+                    val ids = modelOutput.asIntBuffer()
                     try {
-                        Log.d(TAG, output.toString())
-                        for (i in 0 until output.capacity()) {
-                            val probability = output.get(i)
-                            Log.d(TAG, "$probability")
+                        val foodId = mutableListOf<Int>()
+                        for (i in 0 until 10) {
+                            Log.d(TAG, ids.get(i).toString())
+                            foodId.add(i, ids.get(i))
                         }
+                        loadResep(foodId)
                     } catch (e: IOException) {
-                        // File not found?
+                        Log.e(TAG, e.message.toString())
                     }
                 }
             }
     }
 
+    private fun loadResep(foodId: List<Int>) {
+        val foodAdapter = ListFoodAdapter()
+        binding?.rvMenu?.adapter = foodAdapter
+        try {
+            val reader = BufferedReader(
+                InputStreamReader(resources.assets.open("resep.csv"))
+            ).readLines()
+            val foods = mutableListOf<FoodEntity>()
+            var i = 0
+            foodId.map {
+                val label: String = reader[it-1]
+                val food = label.split(";")
+                foods.add(i++, FoodEntity(food[0], food[1], food[2]))
+            }
+            foodAdapter.setData(foods)
+        } catch (e: IOException) {
+            Log.d(TAG, e.message.toString())
+        }
+    }
+
     companion object {
-        private const val TAG = "HomeFragment";
+        private const val TAG = "HomeFragment"
     }
 
     override fun onDestroyView() {
